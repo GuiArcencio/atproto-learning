ddd563f15382a631f24ab5b9394a1d1cc2b902e7
Guilherme Arcencio <guiarcencio@gmail.com>
2024-11-16 00:24:33 -0300

    add pre-commit

--- tests/repo/test_tid.py
+++ tests/repo/test_tid.py
@@ -5,6 +5,7 @@ from freezegun import freeze_time
 from pds.repo.tid import generate_tid
 from pds.utils.encoding import base32_sortable_decode
 
+
 @freeze_time("2024-10-14 13:52")
 def test_tid_timestamp_is_valid():
     tid = generate_tid()
@@ -13,15 +14,17 @@ def test_tid_timestamp_is_valid():
 
     assert time_microseconds == (time_ns() // 1000)
 
+
 def test_tid_format():
     tid = generate_tid()
 
     assert len(tid) == 13
     assert tid.isascii()
 
+
 def test_different_tids_after_1ms():
     tid_1 = generate_tid()
-    sleep(0.001) # 1ms
+    sleep(0.001)  # 1ms
     tid_2 = generate_tid()
 
     assert tid_1 != tid_2
